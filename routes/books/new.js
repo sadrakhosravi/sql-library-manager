213b125c149b329e85bd2c7bf00839e6b2785b56
Sadra Khosravi <sadrakh@outlook.com>
2021-09-12 18:59:22 -0700

    Add: create a new book functionality and form error handling

--- routes/books/new.js
+++ routes/books/new.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const app = express();
 const { Book } = require('../../models');
 const router = express.Router();
 
-// Path relative to /books
+app.use(express.urlencoded({ extended: true }));
 
-/* GET /books page. Displays all the books */
+// Path relative to /books
+/* GET /books/new page. Show new book form */
 router.get('/new', async (req, res, next) => {
+  res.render('new-book');
+});
+
+/* Post /books/new page. Create a new book */
+router.post('/new', async (req, res, next) => {
+  const title = req.body.title;
+  const author = req.body.author;
+  const genre = req.body.genre;
+  const year = req.body.year;
+
+  if (!title || !author || !genre || !year) {
+    res.render('new-book', { isDataValid: false });
+    return;
+  }
+
+  const book = await Book.create({
+    title,
+    author,
+    genre,
+    year,
+  });
+
   res.render('new-book');
 });
 
